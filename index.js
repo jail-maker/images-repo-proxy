7a62a52ea122ac8c1d4f3f988e057e27e0b6d841
Dmitry Marov <d.marov94@gmail.com>
2018-03-23 15:25:34 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,33 +4,24 @@
 
 const config = require('./lib/Config');
 const Koa = require('koa');
-const Router = require('koa-better-router');
+const Proxy = require('koa-proxy');
 
 let app = new Koa();
-let route = Router().loadMethods();
 
-// route.get('/auth-token', async ctx => {
+app.use(async ctx => {
 
-//     try {
+    // check token here
 
-//         let data = { permissions: ['write'] };
-//         let secret = fs.readFileSync(path.resolve(config.privateKeyPath));
-//         let params = { algorithm: 'RS512', expiresIn: config.tokenExpiresIn };
+});
 
-//         let token = jwt.sign(data, secret, params);
-//         ctx.body = { token: token };
+app.use(Proxy({
+    host: config.host + ':' + config.port,
+    map: {
 
-//     } catch (e) {
+        // redirect here
+    
+    
+    }
+}));
 
-//         ctx.response.status(500);
-
-//     }
-
-// });
-
-let apiRoute = Router({ prefix: '/api/v1' });
-apiRoute.extend(route);
-
-app.use(route.middleware());
-app.use(apiRoute.middleware());
 app.listen(config.port);
